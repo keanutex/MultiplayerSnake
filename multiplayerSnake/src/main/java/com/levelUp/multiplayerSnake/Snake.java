4b08601bc1e6d766284ac4d0d4e5c14687a6572a
Keanu Teixeira <keanutex@gmail.com>
2020-02-24 12:39:22 +0200

    More snake logic

--- multiplayerSnake/src/main/java/com/levelUp/multiplayerSnake/Snake.java
+++ multiplayerSnake/src/main/java/com/levelUp/multiplayerSnake/Snake.java
@@ -10,16 +10,36 @@ public class Snake {
         this.snakeSegments = snakeSegments;
     }
 
+    public void createSnake(){
+        snakeSegments.add(new SnakeSegment(3, 3, "up"));
+        snakeSegments.add(new SnakeSegment(3, 4, "up"));
+        snakeSegments.add(new SnakeSegment(3, 5, "up"));
+    }
+
+    public void move(){
+        int x = 0;
+        int y = 0;
+        String direction = snakeSegments.get(0).dir;
+        switch(snakeSegments.get(0).dir){
+            case "up":
+                y  =snakeSegments.get(0).y + 1;
+                break;
+            case "down":
+                y  =snakeSegments.get(0).y - 1;
+                break;
+            case "right":
+                x  =snakeSegments.get(0).x + 1;
+                break;
+            case "left":
+                x  =snakeSegments.get(0).x - 1;
+                break;
+        }
+        snakeSegments.add(0, new SnakeSegment(x, y, direction ));
+        snakeSegments.remove(snakeSegments.size() - 1);
+    }
 
     public void changeDirection(String direction) {
         snakeSegments.get(0).dir = direction;
-
-        int i = 0;
-        for (SnakeSegment s :
-                snakeSegments) {
-            i++;
-            snakeSegments.get(i).dir = snakeSegments.get(i - 1).dir;
-        }
     }
 
     public void addSegment(String direction, int x, int y) {
--- multiplayerSnake/src/main/java/com/levelUp/multiplayerSnake/SnakeSegment.java
+++ multiplayerSnake/src/main/java/com/levelUp/multiplayerSnake/SnakeSegment.java
@@ -6,15 +6,32 @@ public class SnakeSegment {
 
   }
 
+  public int x;
+  public int y;
+  public String dir;
+
   public SnakeSegment(int x, int y, String dir){
     this.x = x;
     this.y = y;
     this.dir = dir;
   }
 
-  public int x;
-  public int y;
-  public String dir;
 
+  public void move(){
+    switch (this.dir){
+      case "up":
+        this.y += 1;
+        break;
+      case "down":
+        this.y -= 1;
+        break;
+      case "right":
+        this.x += 1;
+        break;
+      case "left":
+        this.x -= 1;
+        break;
+    }
+  }
 
 }
