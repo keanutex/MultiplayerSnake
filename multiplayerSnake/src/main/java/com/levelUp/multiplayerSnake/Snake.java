0402adc14f58e66fd2138c9ac55d7cccd16a25a3
Keanu Teixeira <keanutex@gmail.com>
2020-02-24 08:17:00 +0200

    Setting up snake classes

---
+++ multiplayerSnake/src/main/java/com/levelUp/multiplayerSnake/Snake.java
@@ -0,0 +1,11 @@
+package com.levelUp.multiplayerSnake;
+
+import java.util.ArrayList;
+
+public class Snake {
+
+ 
+ public ArrayList<SnakeSegment> snakeSegments = new ArrayList<SnakeSegment>();
+
+
+}
---
+++ multiplayerSnake/src/main/java/com/levelUp/multiplayerSnake/SnakeSegment.java
@@ -0,0 +1,10 @@
+package com.levelUp.multiplayerSnake;
+
+public class SnakeSegment {
+
+  public int x;
+  public int y;
+  public String dir;
+
+
+}
