4692f17b43bc31c53bb6613c8cc5528c590bcf0a
Keanu Teixeira <keanutex@gmail.com>
2020-02-24 08:53:28 +0200

    Working on snake classes

--- multiplayerSnake/src/main/java/com/levelUp/multiplayerSnake/Snake.java
+++ multiplayerSnake/src/main/java/com/levelUp/multiplayerSnake/Snake.java
@@ -3,9 +3,27 @@ package com.levelUp.multiplayerSnake;
 import java.util.ArrayList;
 
 public class Snake {
+    private ArrayList<SnakeSegment> snakeSegments = new ArrayList<SnakeSegment>();
 
- 
- public ArrayList<SnakeSegment> snakeSegments = new ArrayList<SnakeSegment>();
 
+    public Snake(ArrayList<SnakeSegment> snakeSegments) {
+        this.snakeSegments = snakeSegments;
+    }
+
+
+    public void changeDirection(String direction) {
+        snakeSegments.get(0).dir = direction;
+
+        int i = 0;
+        for (SnakeSegment s :
+                snakeSegments) {
+            i++;
+            snakeSegments.get(i).dir = snakeSegments.get(i - 1).dir;
+        }
+    }
+
+    public void addSegment(String direction, int x, int y) {
+        snakeSegments.add(new SnakeSegment(x, y, direction));
+    }
 
 }
--- multiplayerSnake/src/main/java/com/levelUp/multiplayerSnake/SnakeSegment.java
+++ multiplayerSnake/src/main/java/com/levelUp/multiplayerSnake/SnakeSegment.java
@@ -2,6 +2,16 @@ package com.levelUp.multiplayerSnake;
 
 public class SnakeSegment {
 
+  public SnakeSegment(){
+
+  }
+
+  public SnakeSegment(int x, int y, String dir){
+    this.x = x;
+    this.y = y;
+    this.dir = dir;
+  }
+
   public int x;
   public int y;
   public String dir;
