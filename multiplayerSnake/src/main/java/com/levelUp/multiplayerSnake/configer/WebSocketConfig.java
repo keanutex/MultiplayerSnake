513ad43a7020d070e212fea1d386340408fbc6b2
Callum <callum.m.dunne@gmail.com>
2020-03-03 10:47:24 +0200

    Looking into logging service
    L

--- multiplayerSnake/src/main/java/com/levelUp/multiplayerSnake/configer/WebSocketConfig.java
+++ multiplayerSnake/src/main/java/com/levelUp/multiplayerSnake/configer/WebSocketConfig.java
@@ -14,6 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   public void configureMessageBroker(MessageBrokerRegistry config) {
     config.enableSimpleBroker("/snake");
     config.setApplicationDestinationPrefixes("/app");
+    config.enableSimpleBroker("/loggingRes");
   }
 
   @Override
--- multiplayerSnake/src/main/java/com/levelUp/multiplayerSnake/controllers/LoggingController.java
+++ multiplayerSnake/src/main/java/com/levelUp/multiplayerSnake/controllers/LoggingController.java
@@ -1,4 +1,24 @@
 package com.levelUp.multiplayerSnake.controllers;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@Controller
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@RequestMapping("loggingRes")
 public class LoggingController {
+
+    private ArrayList<String> logs = new ArrayList<String>();
+
+    @MessageMapping("/logging")
+    @SendTo("/loggingRes/moveSnakes")
+    public ArrayList<String> getLogging{
+
+    }
+
 }
