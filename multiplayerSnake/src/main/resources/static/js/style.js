913ebd6a5262dab9c42998796ba6eed67e30ab42
Thabiso <magwazathabiso9@gmail.com>
2020-03-05 03:28:50 +0200

    landing-page home page first version added

---
+++ multiplayerSnake/src/main/resources/static/js/style.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const setProp = prop => value => className => {
+  document.querySelector(`.${className}`).style[prop] = value;
+};
+
+document.querySelector(".form-colorpicker").addEventListener("change", e => {
+  const playerColor = e.target.value;
+  const setToPlayerColor = setProp("color")(playerColor);
+
+  setToPlayerColor("btn-submit");
+  setToPlayerColor("form-input");
+  setProp("backgroundColor")(playerColor)("snake-square");
+  setProp("borderColor")(playerColor)("form-input");
+});
