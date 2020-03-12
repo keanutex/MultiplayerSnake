ef1036db74aa10395929ed7101dc38df9c166d9c
Kenneth <kennethw@bbd.co.za>
2020-03-12 13:05:22 +0200

    Base for game two

--- multiplayerSnake/src/main/java/com/levelUp/multiplayerSnake/configer/WebSocketConfig.java
+++ multiplayerSnake/src/main/java/com/levelUp/multiplayerSnake/configer/WebSocketConfig.java
@@ -12,13 +12,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry config) {
-    config.enableSimpleBroker("/snake","/logging","/messaging");
+    config.enableSimpleBroker("/snake","/logging","/messaging","/sync");
     config.setApplicationDestinationPrefixes("/app");
   }
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    registry.addEndpoint("/server").withSockJS();
   }
 
 }
